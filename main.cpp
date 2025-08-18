d58978f1d316975bc57bc66bd4717268c2c3bb3e
Zubialde <xabier.goni.astiz@tipula.com>
2025-08-18 10:17:34 +0200

    Math Support with GLM

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #pragma region Preprocessor directives
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 #include <Shaders/ShaderClass.h>
 
